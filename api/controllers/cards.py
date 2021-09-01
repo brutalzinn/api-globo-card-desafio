db7c8632c130ac7c873cef866b3c2226120a6285
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-09-01 13:45:20 -0300

    lets apply service model now

--- api/controllers/cards.py
+++ api/controllers/cards.py
@@ -11,18 +11,26 @@ class CardClass(Resource):
         cards = mongo.db.cards.find()
         cardList = []
         for c in cards:
-          cardList.append({'id':str(c['_id']),'texto': c['texto'],'tags':c['tags']})
+          if 'tags' in c:
+            cardList.append({'id':str(c['_id']),'texto': c['texto'],'tags':c['tags']})
+          else:
+            cardList.append({'id':str(c['_id']),'texto': c['texto']})
         return jsonify(cardList)
 
     def post(self):
-        req_data = api.payload
-        mongo.db.cards.insert_one(req_data)
+        req = api.payload
+        mongo.db.cards.insert_one(req)
         return ({"message":"Card inserted successfully"}, 200)
     def delete(self,card_id):
         mongo.db.cards.delete_one({"_id":ObjectId(card_id)})
         print('trying to delete',card_id)
         return ({"message":"Card deleted successfully"}, 200)
+    def put(self, card_id):
+        req = api.payload
+        mongo.db.cards.update_one({"_id":ObjectId(card_id)}, {"$set": req})
+        return ({"message":"Card updated successfully"}, 200)
+
 
 
 api.add_resource(CardClass, '/cards/<card_id>')
-api.add_resource(CardClass, '/cards/')
+api.add_resource(CardClass, '/cards')
