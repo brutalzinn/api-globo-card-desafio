f3605a47d45f1f467bd913f6a9ca9937a407d58d
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-09-01 13:19:30 -0300

    delete route for cards

--- api/controllers/cards.py
+++ api/controllers/cards.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, Response
 from flask_restx import Resource
 from server import server
+from bson.objectid import ObjectId
 
 
 app, api, mongo = server.app, server.api, server.mongo
 
-@api.route('/cards')
 class CardClass(Resource):
     def get(self):
         cards = mongo.db.cards.find()
@@ -18,3 +18,11 @@ class CardClass(Resource):
         req_data = api.payload
         mongo.db.cards.insert_one(req_data)
         return ({"message":"Card inserted successfully"}, 200)
+    def delete(self,card_id):
+        mongo.db.cards.delete_one({"_id":ObjectId(card_id)})
+        print('trying to delete',card_id)
+        return ({"message":"Card deleted successfully"}, 200)
+
+
+api.add_resource(CardClass, '/cards/<card_id>')
+api.add_resource(CardClass, '/cards/')
