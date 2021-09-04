770605e97802b2ead7edc3b5ffdf11e4e174b985
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-09-04 15:30:49 -0300

    init documentation

--- routes/route_card.py
+++ routes/route_card.py
@@ -4,7 +4,7 @@ app, api = server.app, server.api
 
 #ADVANCED LIST WITH PAGINATION FOR LIST ALL CARDS
 
-api.add_resource(CardClass, '/v1/cards/list/<page>/<limit>', methods=["POST"])
+api.add_resource(CardClass, '/v1/cards/list/<page>/<limit>', methods=["GET"])
 api.add_resource(CardClass, '/v1/cards/<card_id>', methods=["GET"])
 api.add_resource(CardClass, '/v1/cards', methods=["PUT","DELETE","POST"])
 
--- routes/route_tag.py
+++ routes/route_tag.py
@@ -3,6 +3,6 @@ from controllers.controller_tag import *
 app, api = server.app, server.api
 
 #TAG ROUTE TO MANAGE TAGS
-api.add_resource(TagClass, '/v1/tags/<tag_id>')
+api.add_resource(TagClass, '/v1/tags/<tag_id>', methods=['GET'])
 api.add_resource(TagClass, '/v1/tags')
 
